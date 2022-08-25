22a789c60b7ef703ce12ce9c0fb5b43a8cc110d1
nutan <nutanusadadiya@gmail.com>
2022-08-26 02:09:13 +0530

    understanding controller better
    Signed-off-by: nutan <nutanusadadiya@gmail.com>

--- 03/spring/basicapp/src/main/java/com/scaler/basicapp/HelloController.java
+++ 03/spring/basicapp/src/main/java/com/scaler/basicapp/HelloController.java
@@ -1,14 +1,27 @@
 package com.scaler.basicapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    private ArrayList<String> list = new ArrayList<String>();
+    @GetMapping("/hello/{id}")
+    public String hello(@PathVariable String id){
+        if(list.isEmpty()) {
+            return "Hello World!";
+        }
+
+        String greeting = list.get(Integer.parseInt(id));
+
+        return  greeting;
     }
 
+    @PostMapping("/greetings")
+    public void greetings(@RequestParam String greeting){
+        System.out.println("greeting submitted: " + greeting);
+        list.add(greeting);
+    }
 }
