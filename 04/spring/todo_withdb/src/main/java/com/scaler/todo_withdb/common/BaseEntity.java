2c4ef45a0c92f4fb908276ef64c65da9b9603fe8
nutan <nutanusadadiya@gmail.com>
2022-08-27 18:28:28 +0530

    todos with db.
    Signed-off-by: nutan <nutanusadadiya@gmail.com>

--- 04/spring/todo_withdb/src/main/java/com/scaler/todo_withdb/common/BaseEntity.java
+++ 04/spring/todo_withdb/src/main/java/com/scaler/todo_withdb/common/BaseEntity.java
@@ -1,2 +1,23 @@
-package com.scaler.todo_withdb.common;public class BaseEntity {
+package com.scaler.todo_withdb.common;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @CreatedDate
+    Date createdAt;
+
+    @LastModifiedDate
+    Date updatedAt;
 }
--- 04/spring/todo_withdb/src/main/java/com/scaler/todo_withdb/notes/NoteEntity.java
+++ 04/spring/todo_withdb/src/main/java/com/scaler/todo_withdb/notes/NoteEntity.java
@@ -1,2 +1,26 @@
-package com.scaler.todo_withdb.notes;public class NoteEntity {
+package com.scaler.todo_withdb.notes;
+
+import com.scaler.todo_withdb.common.BaseEntity;
+import com.scaler.todo_withdb.tasks.TaskEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "notes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class NoteEntity extends BaseEntity {
+
+    @Column(name = "title", nullable = false, length = 100)
+    String title;
+
+    @Column(name = "body", nullable = false, length = 1000)
+    String body;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "task_id")
+    TaskEntity task;
 }
